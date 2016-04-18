4cbdb6e32bc9a9c9c097b81d6ea44c57c2e536b2
falco442 <marcofra83@gmail.com>
2016-04-18 11:05:47 +0200

    Aggiunta lingua italiana

---
+++ admin/language/italian/payment/pp_adap.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']      = 'PayPal Adaptive Onlus';
+
+// Text 
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Successo: Hai modificato con successo i dettagli di account del plugin Paypal Adaptive Onlus!';
+$_['text_pp_adap']        = '<a href="https://www.paypal.com/uk/mrb/pal=W9TBB5DTD6QJW" target="_blank"><img src="view/image/payment/paypal.png" alt="PayPal Website Payment Pro" title="PayPal Website Payment Pro" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_authorization'] = 'Authorization';
+$_['text_sale']          = 'Sale';
+
+// Entry
+$_['entry_username']     = 'API Username:';
+$_['entry_password']     = 'API Password:';
+$_['entry_signature']    = 'API Signature:';
+$_['entry_paypal_id']    = 'PayPal ID:';
+$_['entry_test']         = 'Modalità test:<br /><span class="help">Usa il gateway server live o test (sandbox) per processare le transazioni?</span>';
+$_['entry_transaction']  = 'Metodo Transaction';
+$_['entry_total']        = 'Totale:<br /><span class="help">Il totale che l\'ordine deve raggiungere perché il pagamento diventi attivo.</span>';
+$_['entry_order_status'] = 'Status Ordine:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Ordinamento:';
+
+$_['entry_onlus_amount']   = 'Importo da destinare alla onlus (per prodotto):';
+$_['entry_currency_code']  = 'Valuta:';
+
+// Error
+$_['error_permission']   = 'Warning: Non hai i permessi per modificare le impostazioni di Paypal Adaptive Onlus!';
+$_['error_username']     = 'API Username obbligatoria!'; 
+$_['error_password']     = 'API Password obbligatoria!'; 
+$_['error_paypal_id']    = 'PayPal ID obbligatoria!'; 
+$_['error_signature']    = 'API Signature obbligatoria!'; 
+$_['error_onlus_amount'] = 'Importo onlus obbligatorio!'; 
+$_['error_currency_code']= 'Valuta obbligatoria!'; 
+
+$_['text_onlus']		= 'Nominativo Onlus';
+$_['text_onlus_paypal_id']	= 'Paypal ID onlus';
+$_['text_onlus_add']	= 'Aggiunti Onlus';
+?>
