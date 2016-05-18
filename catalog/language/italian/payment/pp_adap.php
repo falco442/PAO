b08a2c1eb051ee7ffc0104d2c1c834c7d3e03dca
falco442 <marcofra83@gmail.com>
2016-05-18 16:31:34 +0200

    Correzione traduzioni

--- catalog/language/italian/payment/pp_adap.php
+++ catalog/language/italian/payment/pp_adap.php
@@ -13,6 +13,6 @@ $_['entry_cc_start_date']  = 'Card Valid From Date:';
 $_['entry_cc_expire_date'] = 'Card Expiry Date:';
 $_['entry_cc_cvv2']        = 'Card Security Code (CVV2):';
 $_['entry_cc_issue']       = 'Card Issue Number:';
-$_['entry_form_onlus_title']   = 'Scegli a quale onlus HOSMESSO donerà:';
+$_['entry_form_onlus_title']   = 'Donazione';
 $_['entry_choose_onlus']   = 'HOSMESSO donerà %s ad una onlus di tua scelta per ogni prodotto che stai acquistando';
 ?>
